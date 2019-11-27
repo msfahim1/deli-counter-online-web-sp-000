ab2125544af47ec922a9259c51dfdd245548e5bf
Michael Fahim <michael_fahim@yahoo.com>
2019-11-27 18:57:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def line(katz_deli)
   if katz_deli.size <1
-    puts " The line is currently empty."
+    puts "The line is currently empty."
   else
     puts " There are #{katz_deli.size} in the line"
 end
